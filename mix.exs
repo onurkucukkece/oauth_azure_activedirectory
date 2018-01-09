758945c4dd1663e61515599b71cd23a0ae508aba
onur <onurkucukkece@gmail.com>
2018-01-09 17:18:17 +0300

    added dependencies

--- mix.exs
+++ mix.exs
@@ -21,8 +21,10 @@ defmodule OauthAzureActivedirectory.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:oauth2, "~> 0.9.2"},
+      {:json, "~> 1.0"},
+      {:json_web_token, "~> 0.2"},
+      {:secure_random, "~> 0.5"},
     ]
   end
 end
