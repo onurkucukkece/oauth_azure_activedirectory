9f4c5b9bddd525946b1abc201198f69fe1b1c3c1
onur <onurkucukkece@gmail.com>
2018-01-14 23:01:16 +0300

    udapted cert path

--- lib/oauth_azure_activedirectory/client.ex
+++ lib/oauth_azure_activedirectory/client.ex
@@ -55,7 +55,7 @@ defmodule OauthAzureActivedirectory.Client do
   end
 
   defp http_request(url) do
-    cacert =  :code.priv_dir(:oauth_azure_activedirectory) ++ '/BaltimoreCyberTrustRoot.crt.pem'
+    cacert =  :code.lib_dir(:oauth_azure_activedirectory) ++ '/BaltimoreCyberTrustRoot.crt.pem'
     :httpc.set_options(socket_opts: [verify: :verify_peer, cacertfile: cacert])
      
     case :httpc.request(:get, {to_charlist(url), []}, [], []) do
