cf9286525b70f55f1ad1ced76f2dbfbb748f2dba
onur <onurkucukkece@gmail.com>
2018-01-16 22:13:49 +0300

    Updated version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OauthAzureActivedirectory.Mixfile do
   def project do
     [
       app: :oauth_azure_activedirectory,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
