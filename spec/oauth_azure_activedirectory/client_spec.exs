aca5995a5101496067de2a3e2e35167746930f58
onur <onurkucukkece@gmail.com>
2018-01-23 21:31:40 +0300

    delete some lets

--- spec/oauth_azure_activedirectory/client_spec.exs
+++ spec/oauth_azure_activedirectory/client_spec.exs
@@ -7,7 +7,6 @@ defmodule OauthAzureActivedirectory.ClientSpec do
   let :id_token, do: File.read "#{File.cwd!}/spec/fixtures/id_token.txt"
 
   # These values were used to create the "successful" id_token JWT.
-  let :client_id, do: 'the client id'
   let :code, do: 'code'
   let :email, do: 'jsmith@contoso.com'
   let :family_name, do: 'smith'
@@ -15,10 +14,8 @@ defmodule OauthAzureActivedirectory.ClientSpec do
   let :issuer, do: 'https://sts.windows.net/bunch-of-random-chars'
   let :kid, do: 'abc123'
   let :name, do: 'John Smith'
-  let :nonce, do: 'my nonce'
   let :session_state, do: 'session state'
   let :auth_endpoint_host, do: 'authorize.com'
-  let :tenant, do: 'tenant' 
   let :openid_config_response, do: "{\"issuer\":\"#{issuer}\",\"authorization_endpoint\":\"http://#{auth_endpoint_host}\",\"jwks_uri\":\"https://login.windows.net/common/discovery/keys\"}" 
   let :keys_response, do: "{\"keys\":[{\"kid\":\"#{kid}\",\"x5c\":[\"#{x5c}\"]}]}" 
   let(:hybrid_flow_params) do
