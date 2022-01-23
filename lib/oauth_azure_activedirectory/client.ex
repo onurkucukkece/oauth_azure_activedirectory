01dc194d21a9be2028a5d926d38129312d876ca8
Onur Kucukkece <onurkucukkece@gmail.com>
2022-01-23 23:02:57 +0100

    make crypto verify work

--- lib/oauth_azure_activedirectory/client.ex
+++ lib/oauth_azure_activedirectory/client.ex
@@ -26,15 +26,19 @@ defmodule OauthAzureActivedirectory.Client do
     ])
   end
 
-  def authorize_url!(params \\ []) do
+  def authorize_url! do
     oauth_session = SecureRandom.uuid
     # NonceAgent.put(oauth_session)
-    
-    params =
-      params
+
+    key = :crypto.strong_rand_bytes(64) |> Base.url_encode64()
+    code_challenge = :crypto.hash(:sha256, key)  |> Base.url_encode64 |> String.replace("=", "")
+
+    params = %{}
       |> Map.update(:response_mode, "form_post", &(&1 * "form_post"))
-      |> Map.update(:response_type, "code id_token", &(&1 * "id_token token"))
-      |> Map.update(:scope, "openid+profile+email", &(&1 * "openid+profile+email"))
+      |> Map.update(:response_type, "code id_token", &(&1 * "code id_token"))
+      |> Map.update(:scope, "openid email", &(&1 * "openid email"))
+      |> Map.update(:code_challenge, code_challenge, &(&1 * code_challenge))
+      |> Map.update(:code_challenge_method, "S256", &(&1 * "S256"))
       |> Map.update(:nonce, oauth_session, &(&1 * oauth_session))
 
     Client.authorize_url!(client(), params)
@@ -45,40 +49,104 @@ defmodule OauthAzureActivedirectory.Client do
   end
 
   def process_callback!(%{params: %{"id_token" => id_token, "code" => code}}) do
-    public =
+    claims = id_token |> String.split(".")
+    
+    header = Enum.at(claims, 0) |> base64_decode
+    payload = Enum.at(claims, 1) |> base64_decode
+    raw_signature = Enum.at(claims, 2) |> Base.url_decode64!(padding: false)
+
+    alg = Map.get(header, "alg")
+    kid = Map.get(header, "kid")
+    chash = Map.get(payload, "c_hash")
+
+    vf = verify_chash(chash, code)
+    vc = verify_client(payload)
+
+    public_PEM =
       jwks_uri()
-      |> get_discovery_keys
+      |> get_discovery_key(kid)
       |> get_public_key
-      |> RsaUtil.public_key
 
-    opts = %{
-      alg: "RS256",
-      key: public
-    }
 
-    {:ok, claims} = JsonWebToken.verify(id_token, opts)
-    verify_token(code, claims)
+    message = Enum.join([Enum.at(claims, 0), Enum.at(claims, 1)], ".")
+
+    size = div(byte_size(raw_signature), 2)
+    <<r::binary-size(size), s::binary-size(size)>> = raw_signature
+    signature = <<48, 129, 136, 2, size, r::binary, 2, size, s::binary>>
+
+    [key_entry] = :public_key.pem_decode(public_PEM)
+    public_key = :public_key.pem_entry_decode(key_entry)
+
+    verif = :public_key.verify(
+      message,
+      :sha256,
+      signature,
+      public_key
+    )
+
+  end
+
+  def base64_decode(string) do
+    {:ok, claim} = string |> Base.url_decode64
+
+    {status, decoded} = JSON.decode claim
+    case status do
+      :ok -> decoded
+      :error -> nil
+    end
+  end
+
+  defp verify_chash(chash, code) do
+    hash_expected = :crypto.hash(:sha256, code)
+
+    {:ok, hash_received } = chash |> Base.url_decode64(padding: false)
+
+    hash_length = byte_size(hash_received)
+    hash_expected = :binary.part(hash_expected, 0, hash_length)
+    hash_expected === hash_received
+  end
+
+  defp verify_client(claims) do
+    configset = config()
+    now = :os.system_time(:second)
+
+    is_valid =
+      # audience
+      configset[:client_id] == Map.get(claims, "aud") and
+      # tenant/issuer
+      configset[:tenant] == Map.get(claims, "tid") and
+      "https://login.microsoftonline.com/#{configset[:tenant]}/v2.0" == Map.get(claims, "iss") and
+      # time checks
+      now < Map.get(claims, "exp") and
+      now >= Map.get(claims, "nbf") and
+      now >= Map.get(claims, "iat") and
+      # nonce
+      # NonceAgent.check_and_delete(claims[:nonce])
+      true
+
+    is_valid
+  end
+
+  defp get_public_key(cert) do
+    spki =
+      "-----BEGIN CERTIFICATE-----\n#{cert}\n-----END CERTIFICATE-----"
+      |> :public_key.pem_decode
+      |> hd
+      |> :public_key.pem_entry_decode
+      |> elem(1)
+      |> elem(7)
+
+    :public_key.pem_entry_encode(:SubjectPublicKeyInfo, spki)
+    |> List.wrap
+    |> :public_key.pem_encode
   end
 
   defp config do
     Application.get_env(:oauth_azure_activedirectory, OauthAzureActivedirectory.Client)
   end
 
-  defp jwks_uri do
-    {status, list} =
-      open_id_configuration()
-      |> http_request
-      |> JSON.decode
-
-    if status == :ok do
-      list["jwks_uri"]
-    else
-      nil
-    end
-  end
-
   defp http_request(url) do
-    cacert =  :code.priv_dir(:oauth_azure_activedirectory) ++ '/BaltimoreCyberTrustRoot.crt.pem'
+    cacert =  :code.priv_dir(:oauth_azure_activedirectory) ++ '/DigiCertGlobalRootCA.crt.pem'
     :httpc.set_options(socket_opts: [verify: :verify_peer, cacertfile: cacert])
 
     case :httpc.request(:get, {to_charlist(url), []}, [], []) do
@@ -89,74 +157,30 @@ defmodule OauthAzureActivedirectory.Client do
     end
   end
 
-  defp get_discovery_keys(url)do
+  defp get_discovery_key(url, kid) do
     {status, list} = 
       url
       |> http_request
       |> JSON.decode
 
+    key = Enum.find(list["keys"], fn elem -> Logger.info elem["kid"]; elem["kid"] == kid end)
     case status do
-      :ok -> Enum.at(list["keys"], 0)["x5c"]
+      :ok -> key["x5c"]
       :error -> nil
     end
-  end
-
-  defp get_public_key(cert) do
-    spki =
-      "-----BEGIN CERTIFICATE-----\n#{cert}\n-----END CERTIFICATE-----\n"
-      |> :public_key.pem_decode
-      |> hd
-      |> :public_key.pem_entry_decode
-      |> elem(1)
-      |> elem(7)
-    
-    :public_key.pem_entry_encode(:SubjectPublicKeyInfo, spki)
-    |> List.wrap
-    |> :public_key.pem_encode
   end
 
   defp open_id_configuration do
     "https://login.microsoftonline.com/common/.well-known/openid-configuration"
   end
 
+  defp jwks_uri do
+    "https://login.microsoftonline.com/common/discovery/keys"
+  end
+
   defp verify_token(code, claims) do
     claims
     |> verify_chash(code)
     |> verify_client
-  end
-
-  defp verify_chash(claims, code) do
-    hash_actual = :crypto.hash(:sha256, code)
-
-    {:ok, hash_expected } =
-      claims[:c_hash]
-      |> Base.url_decode64(padding: false)
-
-    hash_length = byte_size(hash_expected)
-    hash_actual = :binary.part(hash_actual, 0, hash_length)
-    ^hash_actual = hash_expected
-
-    claims
-  end
-
-  defp verify_client(claims) do
-    configset = config()
-    now = :os.system_time(:second)
-
-    is_valid =
-      # audience
-      configset[:client_id] == claims[:aud] and
-      # tenant/issuer
-      configset[:tenant] == claims[:tid] and
-      "https://sts.windows.net/#{configset[:tenant]}/" == claims[:iss] and
-      # time checks
-      now < claims[:exp] and
-      now >= claims[:nbf] and
-      now >= claims[:iat] and
-      # nonce
-      # NonceAgent.check_and_delete(claims[:nonce])
-      true
-    true = is_valid
-    claims
   end
 end
