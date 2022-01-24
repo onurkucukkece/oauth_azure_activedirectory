e7f16f6585e96a35ed0102e117926ac8dd5a1513
Onur Kucukkece <onurkucukkece@gmail.com>
2022-01-24 22:24:07 +0100

    Boyscou

--- lib/oauth_azure_activedirectory/client.ex
+++ lib/oauth_azure_activedirectory/client.ex
@@ -3,7 +3,7 @@ defmodule OauthAzureActivedirectory.Client do
   alias OAuth2.Strategy.AuthCode
 
   def logout(redirect_uri) do
-  	configset = config()
+    configset = config()
     tenant = configset[:tenant]
 
     logout_url = openid_configuration("end_session_endpoint", tenant)
@@ -18,7 +18,6 @@ defmodule OauthAzureActivedirectory.Client do
       client_id: configset[:client_id],
       client_secret: configset[:client_secret],
       redirect_uri: configset[:redirect_uri],
-      
       authorize_url: openid_configuration("authorization_endpoint", configset[:tenant]),
       token_url: openid_configuration("token_endpoint", configset[:tenant])
     ])
@@ -29,13 +28,14 @@ defmodule OauthAzureActivedirectory.Client do
     key = :crypto.strong_rand_bytes(64) |> Base.url_encode64()
     code_challenge = :crypto.hash(:sha256, key)  |> Base.url_encode64 |> String.replace("=", "")
 
-    params = %{}
-      |> Map.update(:response_mode, "form_post", &(&1 * "form_post"))
-      |> Map.update(:response_type, "code id_token", &(&1 * "code id_token"))
-      |> Map.update(:scope, "openid email", &(&1 * "openid email"))
-      |> Map.update(:code_challenge, code_challenge, &(&1 * code_challenge))
-      |> Map.update(:code_challenge_method, "S256", &(&1 * "S256"))
-      |> Map.update(:nonce, oauth_session, &(&1 * oauth_session))
+    params = %{
+      response_mode: "form_post",
+      response_type: "code id_token",
+      scope: "openid email",
+      code_challenge: code_challenge,
+      code_challenge_method: "S256",
+      nonce: oauth_session
+    }
 
     Client.authorize_url!(client(), params)
   end
@@ -47,9 +47,10 @@ defmodule OauthAzureActivedirectory.Client do
   def process_callback!(%{params: %{"id_token" => id_token, "code" => code}}) do
     claims = id_token |> String.split(".")
     
-    header = Enum.at(claims, 0) |> base64_decode
-    payload = Enum.at(claims, 1) |> base64_decode
+    header = Enum.at(claims, 0) |> Base.url_decode64! |> JSON.decode!
+    payload = Enum.at(claims, 1) |> Base.url_decode64! |> JSON.decode!
     signature = Enum.at(claims, 2) |> Base.url_decode64!(padding: false)
+
     message = Enum.join([Enum.at(claims, 0), Enum.at(claims, 1)], ".")
 
     kid = Map.get(header, "kid")
@@ -60,16 +61,6 @@ defmodule OauthAzureActivedirectory.Client do
     vts = verify_token_signature(message, signature, kid)
 
     payload
-  end
-
-  def base64_decode(string) do
-    {:ok, claim} = string |> Base.url_decode64
-
-    {status, decoded} = JSON.decode claim
-    case status do
-      :ok -> decoded
-      :error -> nil
-    end
   end
 
   defp verify_chash(chash, code) do
