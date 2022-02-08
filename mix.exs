de628218ae69828fb21978d4230e9f77151a43a8
Onur Kucukkece <onurkucukkece@gmail.com>
2022-02-08 21:15:29 +0100

    Update coveralls to circleci in preferred cli env settings

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule OauthAzureActivedirectory.Mixfile do
       package: package(),
       description: description(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.circle": :test, "coveralls.html": :test]
     ]
   end
 
