175d7eec48603ea0615f6c6d931864ed94019b9c
Onur Kucukkece <onurkucukkece@gmail.com>
2023-07-23 12:32:15 +0200

    Bump version to V1.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OauthAzureActivedirectory.Mixfile do
   def project do
     [
       app: :oauth_azure_activedirectory,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env == :prod,
       deps: deps(),
