269c876d90c25a4d26823240168a771a0c588767
Onur Kucukkece <onurkucukkece@gmail.com>
2025-12-02 20:27:01 +0100

    Update secure_random

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule OauthAzureActivedirectory.Mixfile do
     [
       {:oauth2, "~> 2.0"},
       {:json, "~> 1.0"},
-      {:secure_random, "~> 0.5"},
+      {:secure_random, "~> 0.5.1"},
       {:ex_doc, "~> 0.19", only: :dev},
       {:mock, "~> 0.3.0", only: :test},
       {:excoveralls, "~> 0.10", only: :test},
